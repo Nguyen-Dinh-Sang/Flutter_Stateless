6a35a7b003bf9151238376d2c6b5f05a97008b81
DinhSang <dinhsangdroid@gmail.com>
2021-03-26 11:07:04 +0700

    Stateless-DinhSang: final

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // cơ bản
@@ -10,15 +11,83 @@ import 'package:flutter/material.dart';
 //   );
 // }
 
-void main() {
-  runApp(MyApp());
-}
+// nâng cấp 1 xíu
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text('hello world', textDirection: TextDirection.ltr,),
+//     );
+//   }
+// }
 
-class MyApp extends StatelessWidget {
+class MyAppBar extends StatelessWidget {
+  MyAppBar(this.title) {
+  }
+
+  final Widget title;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('hello world', textDirection: TextDirection.ltr,),
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      //các thuộc tính khác
+      decoration: BoxDecoration(color: Colors.blue),
+      //widget bên trong theo chiều ngang.
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            //rê chuột vào nó lên
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          //lấp đầy khoảng trống còn lại
+          Expanded(
+              child: title,
+          ),
+          IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null)
+        ],
+      ),
     );
   }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      //trải view theo chiều dọc
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            Text(
+              'Title',
+              style: Theme.of(context).primaryTextTheme.title,
+            )
+          ),
+          Expanded(
+              child: Center(
+                child: Text("Hello"),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: 'My App',
+    home: MyScaffold(),
+  ));
 }
